d8947cd3129256855a980043bc3ae0dd81b10594
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-10-22 16:10:10 +0300

    code enhancements

--- script.js
+++ script.js
@@ -4,32 +4,53 @@ let InitialTodos = [
     {id: 2, title: "learn HTML", done: false},
     {id: 3, title: "laufe", done: false},
 ];
-if (!localStorage.getItem('todos')){
+if (!localStorage.getItem('todos')) {
     localStorage.setItem('todos', JSON.stringify(InitialTodos));
 }
 
 let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
-function createTodoItems(todoData) {
+// DOM node generator
+function createTodoItem(todoData) {
     // <li><input type="checkbox"/> <label>Clean up</label></li>
     const li = document.createElement('li');
+
     const input = document.createElement('input');
     input.type = 'checkbox'
 
     const label = document.createElement('label');
-
     const labelTitle = document.createTextNode(todoData.title);
     label.appendChild(labelTitle)
 
+    //Style the todos based on status
+    updateCheckedFormElement(todoData, label, input);
+
+    //listen for any status updates
+    input.addEventListener("change", function () {
+        todoData.done = input.checked //false or true
+        localStorage.setItem('todos', JSON.stringify(todos))
+        renderTodos(todos)
+    })
+
+
     li.append(input, label);
     return li;
+}
+function updateCheckedFormElement (todoItem, label, input) {
+    if (todoItem.done) {
+        label.style.textDecoration = 'line-through'
+        input.checked = true
+    } else {
+        label.style.textDecoration = 'none'
+        input.checked = false
+    }
 }
 
 function renderTodos(todos) {
     document.querySelector('.ul-wrapper').innerHTML = '';
 
     for (let todoData of todos) {
-        const newTodDoItem = createTodoItems(todoData);
+        const newTodDoItem = createTodoItem(todoData);
         document.querySelector('.ul-wrapper').appendChild(newTodDoItem)
     }
 }
@@ -44,10 +65,12 @@ function addTodoItem(event) {
     renderTodos(todos)
 }
 
-document.querySelector('form').addEventListener('submit', addTodoItem);
 
+document.querySelector('form').addEventListener('submit', addTodoItem);
 
 renderTodos(todos)
 
 //reload the page and persist data
 //checkboxes need to work
+
+
